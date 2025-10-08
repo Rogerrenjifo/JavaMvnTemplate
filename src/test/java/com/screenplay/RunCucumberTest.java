1224b9cc99635bd1ab6645e12a5f9a6db2af0986
rrenjifo <rrenjifo@loftware.com>
2025-10-07 21:31:31 -0400

    add first template

---
+++ src/test/java/com/screenplay/RunCucumberTest.java
@@ -0,0 +1,15 @@
+package com.screenplay;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("com/screenplay")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, pretty ")
+public class RunCucumberTest {
+}
